594abcdcb97ee300918809a1678713cb2bb9ed6d
Noriyuki Isaka <nor51010@users.noreply.github.com>
2019-10-28 11:31:38 +0900

    create HELLO.cbl
    upload test target programs

---
+++ HELLO.cbl
@@ -0,0 +1,9 @@
+       IDENTIFICATION              DIVISION.
+       PROGRAM-ID.                 HELLO.
+       DATA                        DIVISION.
+       WORKING-STORAGE             SECTION.
+       PROCEDURE                   DIVISION.
+       MAIN-RTN.
+           DISPLAY "HELLO WORLD!".
+       MAIN-EXT.
+           STOP RUN.
